7f7c0444deaf4e646426af8623293ec754a40d4d
latifi777 <abdelalilatifi3@gmail.com>
2024-11-27 17:20:04 +0100

    en train d'afficher les joueurs

--- main.js
+++ main.js
@@ -13,7 +13,40 @@ fetch('/players.json')
     console.error('Erreur:', erreur);
 })
 
+function displayplayers(){
+const showplayers = document.createElement('div');
+showplayers.innerHTML = `
+ <div id="showplayer" class="card">
+        <div class="firt_part">
+        <div class="informations">
+        <div>${position}</div>
+        <div>${nationality}</div>
+        <img src="${flag}" alt="">
+        <div>${club}</div>
+        <img src="${logo}" alt="">
+        </div>
+        <div class="player_img">
+            <img src="${photo}" alt="">
+        </div>
+        </div>
+        <div class="second_part">
+            <div>
+                <div>"rating": ${rating}</div><br>
+                <div>"pace": ${pace}</div><br>
+                <div>  "shooting": ${shooting}</div><br>
+            </div>
+            <div>
+                <div>"passing": ${passing}</div><br>
+                <div>"dribbling": ${dribbling}</div><br>
+                <div>"defending": ${defending}</div><br>
+                 <div>"physical": ${physical}</div>
 
+            </div>
+
+        </div>
+`
+document.getElementById('player').appendChild(showplayers) 
+}
 
 
 
