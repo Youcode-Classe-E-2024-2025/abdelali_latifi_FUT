ec1c1306ccde520dd6a465552b829fd45a089c20
latifi777 <abdelalilatifi3@gmail.com>
2024-11-29 09:28:08 +0100

    la possibilté de montrer tous les joueurs

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const players = document.getElementById('players');
 const addplayer = document.querySelectorAll('.emptycard');
 const pop_up = document.getElementById('pop_up');
 const EXITE = document.getElementById('EXITE');
+const SHOW_ALL_PLAYERS = document.getElementById('SHOW_ALL_PLAYERS'); 
 let stars = [];
 let selectedPlayers = [];
 
@@ -187,4 +188,78 @@ addplayer.forEach(element => {
             pop_up.style.display = 'block';
         }
     });
+});
+
+function displayallplayers() {
+    players.innerHTML = ""; 
+
+    stars.players.forEach(player => {
+        const showallplayers = document.createElement('div');
+        showallplayers.classList.add('card');
+
+        if (player.position !== 'GK') {
+            showallplayers.innerHTML = `
+                <div class="player_name">${player.name}</div>
+                <div class="firt_part">
+                    <div>
+                        <div>${player.position}</div><br>
+                        <img class='flag' src="${player.flag}" alt=""><br><br>
+                        <div>${player.club}</div>
+                    </div>
+                    <div class="player_img">
+                        <img class='playerphoto' src="${player.photo}" alt="">
+                    </div>
+                </div>
+                <div class="second_part">
+                    <div>
+                        <span>passing: ${player.passing}</span><br><br>
+                        <span>pace: ${player.pace}</span><br><br>
+                        <span>shooting: ${player.shooting}</span>
+                    </div>
+                    <div>
+                        <span>dribbling: ${player.dribbling}</span><br><br>
+                        <span>defending: ${player.defending}</span><br><br>
+                        <span>physical: ${player.physical}</span>
+                    </div>
+                </div>
+            `;
+        } else {
+            showallplayers.innerHTML = `
+                <div class="player_name">${player.name}</div>
+                <div class="firt_part">
+                    <div>
+                        <div>${player.position}</div><br>
+                        <img class='flag' src="${player.flag}" alt=""><br><br>
+                        <div>${player.club}</div>
+                    </div>
+                    <div class="player_img">
+                        <img class='playerphoto' src="${player.photo}" alt="">
+                    </div>
+                </div>
+                <div class="second_part">
+                    <div>
+                        <span>diving: ${player.diving}</span><br><br>
+                        <span>handling: ${player.handling}</span><br><br>
+                        <span>kicking: ${player.kicking}</span>
+                    </div>
+                    <div>
+                        <span>reflexes: ${player.reflexes}</span><br><br>
+                        <span>speed: ${player.speed}</span><br><br>
+                        <span>positioning: ${player.positioning}</span>
+                    </div>
+                </div>
+            `;
+        }
+
+        players.appendChild(showallplayers);
+
+        showallplayers.addEventListener('click', () => {
+            chooseplayer(player);
+        });
+    });
+}
+
+SHOW_ALL_PLAYERS.addEventListener('click', () => {
+    displayallplayers();
+    pop_up.style.display = 'block'; 
 });
